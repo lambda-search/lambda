1d0d4e175769156d26fa48d8b20fe63101a6284c
1552739156@qq.com <110255362+tiger631@users.noreply.github.com>
2022-09-25 14:18:33 +0800

    fix

--- lambda/graph/percentile_stats.h
+++ lambda/graph/percentile_stats.h
@@ -18,47 +18,47 @@
 #include "parameters.h"
 
 namespace lambda {
-  struct query_stats {
-    double total_us = 0;  // total time to process query in micros
-    double io_us = 0;     // total time spent in IO
-    double cpu_us = 0;    // total time spent in CPU
+    struct query_stats {
+        double total_us = 0;  // total time to process query in micros
+        double io_us = 0;     // total time spent in IO
+        double cpu_us = 0;    // total time spent in CPU
 
-    unsigned n_4k = 0;          // # of 4kB reads
-    unsigned n_8k = 0;          // # of 8kB reads
-    unsigned n_12k = 0;         // # of 12kB reads
-    unsigned n_ios = 0;         // total # of IOs issued
-    unsigned read_size = 0;     // total # of bytes read
-    unsigned n_cmps_saved = 0;  // # cmps saved
-    unsigned n_cmps = 0;        // # cmps
-    unsigned n_cache_hits = 0;  // # cache_hits
-    unsigned n_hops = 0;        // # search hops
-  };
+        unsigned n_4k = 0;          // # of 4kB reads
+        unsigned n_8k = 0;          // # of 8kB reads
+        unsigned n_12k = 0;         // # of 12kB reads
+        unsigned n_ios = 0;         // total # of IOs issued
+        unsigned read_size = 0;     // total # of bytes read
+        unsigned n_cmps_saved = 0;  // # cmps saved
+        unsigned n_cmps = 0;        // # cmps
+        unsigned n_cache_hits = 0;  // # cache_hits
+        unsigned n_hops = 0;        // # search hops
+    };
 
-  template<typename T>
-  inline T get_percentile_stats(
-      query_stats *stats, uint64_t len, float percentile,
-      const std::function<T(const query_stats &)> &member_fn) {
-    std::vector<T> vals(len);
-    for (uint64_t i = 0; i < len; i++) {
-      vals[i] = member_fn(stats[i]);
+    template<typename T>
+    inline T get_percentile_stats(
+            query_stats *stats, uint64_t len, float percentile,
+            const std::function<T(const query_stats &)> &member_fn) {
+        std::vector<T> vals(len);
+        for (uint64_t i = 0; i < len; i++) {
+            vals[i] = member_fn(stats[i]);
+        }
+
+        std::sort(vals.begin(), vals.end(),
+                  [](const T &left, const T &right) { return left < right; });
+
+        auto retval = vals[(uint64_t) (percentile * len)];
+        vals.clear();
+        return retval;
     }
 
-    std::sort(vals.begin(), vals.end(),
-              [](const T &left, const T &right) { return left < right; });
-
-    auto retval = vals[(uint64_t)(percentile * len)];
-    vals.clear();
-    return retval;
-  }
-
-  template<typename T>
-  inline double get_mean_stats(
-      query_stats *stats, uint64_t len,
-      const std::function<T(const query_stats &)> &member_fn) {
-    double avg = 0;
-    for (uint64_t i = 0; i < len; i++) {
-      avg += (double) member_fn(stats[i]);
+    template<typename T>
+    inline double get_mean_stats(
+            query_stats *stats, uint64_t len,
+            const std::function<T(const query_stats &)> &member_fn) {
+        double avg = 0;
+        for (uint64_t i = 0; i < len; i++) {
+            avg += (double) member_fn(stats[i]);
+        }
+        return avg / len;
     }
-    return avg / len;
-  }
 }  // namespace lambda
